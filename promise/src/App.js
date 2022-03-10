e3e72ca90393f179a43446a33939e24a7e835bc6
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-03-10 12:41:07 +0100

    Update App.js

--- promise/src/App.js
+++ promise/src/App.js
@@ -1,31 +1,32 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css'
 import { useState } from "react";
 
 /* 2 post primi due 5 commenti non adiacenti 10 user*/
 //https://jsonplaceholder.typicode.com/posts/1
-async function fetchPost() {
+async function fetchPost(ID) {
   console.log('sto facendo la chiamata api')
   const post = await fetch(
-    'https://jsonplaceholder.typicode.com/posts/1',
+    'https://jsonplaceholder.typicode.com/posts/'+ID,
 
     {
       method: 'GET'
     }
   ).then((p) => p.json());
-  console.log(post)
+  //console.log(post)
   return post
 
 }
-async function fetchCommenti() {
+async function fetchCommenti(ID) {
   const commenti = await fetch(
-    'https://jsonplaceholder.typicode.com/comments/1',
+    'https://jsonplaceholder.typicode.com/comments/'+ID,
 
     {
       method: 'GET'
     }
   ).then((c) => c.json());
-  console.log(commenti)
+  //console.log(commenti)
   return commenti
 }
 async function fetchUtenti() {
@@ -59,6 +60,16 @@ function getKeys2(utenti) {
   }
 }
 
+const random = (lun, maxN) => {
+  let arr = [];
+  while(arr.length < lun){
+      var r = Math.floor(Math.random() * maxN) + 1;
+      if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  console.log(arr); 
+  return arr
+}
+
 
 
 
@@ -66,41 +77,53 @@ function App() {
   const [post, setPost] = useState([])
   const [commenti, setCommenti] = useState([])
   const [utenti, setUtenti] = useState([])
-  const buttonClickPost = () => {
-    fetchPost().then((result) => {
-      setPost([result])
-      console.log(result)
-    })
+  const buttonClickPost = async () => {
+    let posts = []
+    for(let i = 1 ; i <= 2; i++){
+      let post = await fetchPost(i).then((result) => result)
+      posts.push(post)
+    }
+    setPost(posts)
   }
-  const keys = getKeys(post)
-  console.log(keys)
-  const buttonClickCommenti = () => {
-    fetchCommenti().then((result) => {
-      setCommenti([result])
-      console.log(result)
-    })
+
+  const buttonClickComments = async () => {
+    let comments = []
+    let ids = random(3, 500)
+    for(let i = 0 ; i < ids.length; i++){
+      let comment = await fetchCommenti(ids[i]).then((result) => result)
+      comments.push(comment)
+    }
+    setCommenti(comments)
   }
-  const keys1 = getKeys1(commenti)
-  console.log(keys1)
-  const buttonClickUtenti = () => {
-    fetchUtenti().then((result) => {
-      setUtenti([result])
-      console.log(result)
-    })
-  }
-  const keys2 = getKeys2(utenti)
-  console.log(keys2)
+  // const keys = getKeys(post)
+  // console.log(keys)
+  // const buttonClickCommenti = () => {
+  //   fetchCommenti().then((result) => {
+  //     setCommenti([result])
+  //     console.log(result)
+  //   })
+  // }
+  // const keys1 = getKeys1(commenti)
+  // console.log(keys1)
+  // const buttonClickUtenti = () => {
+  //   fetchUtenti().then((result) => {
+  //     setUtenti([result])
+  //     console.log(result)
+  //   })
+  // }
+  // const keys2 = getKeys2(utenti)
+  // console.log(keys2)
 
   return (
     <div className="App">
       <header className="App-header">
-        <button type='button' onClick={buttonClickPost}>API post</button>
-        <button type='button' onClick={buttonClickCommenti}>API commenti</button>
-        <button type='button' onClick={buttonClickUtenti}>API utenti</button>
+        <button type='button' onClick={() => buttonClickPost()}>API post</button>
+        <button type='button' onClick={() => buttonClickComments()}>API commenti</button>
+        {/* <button type='button' onClick={buttonClickUtenti}>API utenti</button> */}
         <br>
         </br>
         <div>
-          <table>
+          {/* <table>
             <thead>
               <tr>
                 {keys.map((key) => {
@@ -110,28 +133,33 @@ function App() {
             </thead>
             <tbody>
               <pre>{JSON.stringify({post})}</pre>
-              
+               */}
+              <ul>
               {post.map((item) => {
                 return (
-                  <tr key={item.id}>
-                    {item.title}
-                    {keys.map((key, index) => {
-                      const value = item[key];
-                      if ( typeof value === 'string')
-                        return <td key={index}>{value}</td>
-                      else return ""
-                    })}
-                  </tr>
-
+                  <li key={item.id}>
+                    {item.body}
+                  </li>
                 )
               })}
-              {post.filter((item,el)=>{
+              </ul>
+
+              <ul>
+              {commenti.map((item) => {
+                return (
+                  <li key={item.id}>
+                    {item.body}
+                  </li>
+                )
+              })}
+              </ul>
+              {/* {post.filter((item,el)=>{
                 const value = item[el]
                 if( value > item.id[el] === '1' && value < item.id[el] === '4' )
                 return <tr key={item.body}>  
                 </tr>
-              })}
-
+              })} */}
+{/* 
             </tbody>
           </table>
         </div>
@@ -205,9 +233,9 @@ function App() {
               })}
 
               </tbody>
-            </table>
+            </table> */}
           </div>
-        </div>
+
       </header>
     </div>
   );
